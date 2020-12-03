1f19b1483a4016aaf1228ce239ecdda51a3dbf63
Atri Hegde <iamatrihegde@gmail.com>
2020-12-03 23:14:50 +0000

    moving from String array to .txt file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 
+/*
+This project is highly undeveloped and not finished, many little details like, exploration is totally random and you cannot end in the same place through retracing steps.
+Surroundings have no impact on the actual game, surroundings aren't real, you aren't gonna find a river in front of you nor is it gonna affect the game
+*/
+
 public class Main {
 
+    public static String scenaryDirectory;
     int[] stats = new int[5];
     int[] inventory = new int[6];
     int hp = 100;
@@ -39,6 +45,7 @@ public class Main {
 
     }
 
+
     //This displays the letters they need to enter to perform actions in the game
     public static void keyBinds(){
         System.out.println("-------------------------");
@@ -55,15 +62,46 @@ public class Main {
 
 
     //will perform actions for inputs like fight, sword, block, dodge, heavy attack
-    public static void fightMode(char key){
-
+    public static void fightMode(){
+        char key = input.nextLine().charAt(0);
+        switch(key){
+            case('r'):
+                rules();
+            case('i'):
+                displayInventory();
+            case('l'):
+            case('h'):
+            case('d'):
+            case('s'):
+            default:
+                System.out.println("Invalid Input!!");
+                break;
+        }
 
 
 
     }
 
+    //This method sets the directory of the scenary.txt thats used in the next method.
+    public static void setScenaryDirectory(){
+        scenaryDirectory = System.getProperty("user.dir") + "\\scenary.txt";
+    }
+
+
+
     //Moves the user to different locations and provides a brief description of his surroundings
     public static void move(String direction){
+        //The string array has random description of surrounding that are chosen at random to show to the user
+//        String[] surroundings = {"You stand in a flat region dotted with huge multicolored stones. It's also spotted with trees, grass, and foul-smelling wildflowers.\nYou can see a huge river close by and a citadel to the south.\nThe temperature is quite cool and the sky is overcast."
+//                , "You stand in a mountainous region strewn with small shrubs.\nYou can see a lake nearby.\nThe temperature is very warm and the sky is mostly clear."
+//                , "You stand in a hilly region sprinkled with smooth multicolored stones.\nYou can see a bog nearby.\nThe temperature is hot and the sky is overcast."
+//                , "You stand in a broken terrain smattered with unusual grass and various wildflowers.\nYou can see a stream in the distance.\nThe temperature is a little cool and the sky is partially cloudy."
+//                , "You stand in a hilly terrain covered with huge purplish stones.\nYou can see a village nearby.\nThe temperature is hot and the sky is overcast."
+//                , "You stand in a mountainous region sprinkled with foul-smelling trees, dead shrubs, and grass.\nYou can see a small body of water to the south and a cave to the north.\nThe temperature is quite cool and the sky is overcast."
+//                , "You stand in a mountainous terrain strewn with gigantic bluish rocks.  You can see a bog miles away and a monastery nearby.  The temperature is very warm and the sky is overcast."
+//                , "You stand in a mountainous region spotted with grass and wildflowers.  You can see a large lake miles away.  The temperature is somewhat warm and the sky is partially cloudy."
+//                , "You stand in a flat area sprinkled with huge brown stones.  You can see a tower to the north.  The temperature is warm and the sky is partially cloudy."
+//                , "You stand in a mountainous terrain replete with small shrubs.  You can see a small body of water close by and a citadel to the east.  The temperature is quite cool and the sky is partially cloudy."};
 
     }
 
